d93ab774f84a2d7c71a49f809d56420c8ed1d252
Rick <rahelioheynneman@gmail.com>
2025-07-27 12:18:27 +0100

    updated base url

--- python_app/app.py
+++ python_app/app.py
@@ -9,8 +9,9 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/travel-calculator/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel_costs.db'
+app.config['APPLICATION_ROOT'] = '/travel-calculator'
 db = SQLAlchemy(app)
 
 def format_postcode(postcode):
